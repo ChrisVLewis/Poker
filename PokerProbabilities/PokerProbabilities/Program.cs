5ce1d94efe8f5f51e5a1496cb1c7a503237e439e
Christopher Lewis <christophvlewis@gmail.com>
2020-07-03 08:01:48 -0400

    Initial Commit PokerProbabilities

---
+++ PokerProbabilities/PokerProbabilities/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace PokerProbabilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PokerProbabilities");
+            string MongoDB_url = "localhost:27017/Poker";
+
+        }
+    }
+}
